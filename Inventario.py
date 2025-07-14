33ef94c6e78fd74960dc21d5c9af87153d12119d
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-07-14 11:25:12 -0600

    Inventario

--- Inventario.py
+++ Inventario.py
@@ -1,3 +1,10 @@
 productos={}
 cantidad=int(input("Cantidad de productos que desea ingresar: "))
 for i in range(cantidad):
+    print(f"Cantidad {i+1}")
+    codigo=input("Ingrese el código: ")
+    nombredelproducto=input("Ingrese el nombre: ")
+    caregoria=input("Ingrese la caregoria: ")
+    talla=input("Ingrese la talla: ")
+    preciounitario=input("Ingrese la preciounitario: ")
+    cantidadenstock=int(input("Ingrese la cantidad en stock: "))
