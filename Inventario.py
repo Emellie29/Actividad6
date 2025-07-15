dd95238696f7faec8637c55d89cf6d8dd2b80805
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-07-14 18:35:14 -0600

    inventario

--- Inventario.py
+++ Inventario.py
@@ -29,6 +29,29 @@ for i in range(cantidad):
         print("Ingrese una cantidad entera positiva")
         stock=input("Cantidad en stock: ")
 
+print("\nInventario:")
+for codigo, datos in producto.items():
+    print(f"Código: {codigo}")
+    print(f"Nombre: {datos['nombre']}")
+    print(f"Categoria: {datos['categoria']}")
+    print(f"Talla: {datos['talla']}")
+    print(f"Precio unidad: Q.{datos['precio']:.2f}")
+    print(f"Stock: {datos['stock']}")
+
+print("\nBúsqueda")
+busqueda=input("Ingrese el código del producto: ").strip()
+if busqueda in producto:
+    print(f"Producto encotrado.")
+    datos=producto[busqueda]
+    print(f"Nombre: {datos['nombre']}")
+    print(f"Categoria: {datos['categoria']}")
+    print(f"Talla: {datos['talla']}")
+    print(f"Precio unidad: Q.{datos['precio']}")
+    print(f"Stock: {datos['stock']}")
+else:
+    print("Producto no encontrado")
+
+
     producto[codigo]={
         "nombre_producto":nombre_producto,
         "categoria":categoria,
